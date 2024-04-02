2a9ac491123a30c4070e01a5186e44d3a13890f5
Skeletonek <skeleton0199@gmail.com>
2024-04-02 21:26:41 +0200

    Add QProcess starting ffmpeg and some basic UI

--- Main.qml
+++ Main.qml
@@ -1,11 +1,38 @@
 import QtQuick
+import QtQuick.Controls
 
-Window {
+ApplicationWindow {
     width: 640
     height: 480
     visible: true
-    title: qsTr("Hello World")
-    Text {
-        text: "Hi there!"
+    title: qsTr("GuineaMPEG")
+    Column {
+        objectName: "col"
+        anchors.centerIn: parent
+        width: parent.width
+
+        ScrollView {
+            objectName: "sv"
+            width: parent.width
+            height : 400
+            TextArea {
+                text: "n/a"
+                objectName: "textOutput"
+                width: parent.width
+            }
+        }
+
+        Text {
+            color: "#ffffff"
+            id: testLabel
+            text: "Test 1"
+        }
+
+        Button {
+            text: "Ok"
+            onClicked: function() {
+                testLabel.text = "Test 2"
+            }
+        }
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QProcess>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +16,21 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(url);
+
+    QProcess process;
+    process.start("ffmpeg", QStringList() << "-help");
+    process.waitForFinished();
+    QByteArray sout = process.readAllStandardOutput();
+    std::cout << sout.toStdString();
+
+    QObject *root = engine.rootObjects()[0];
+    QObject *col = root->findChild<QObject*>("col");
+    QObject *sv = root->findChild<QObject*>("sv");
+    QObject *textOutput = sv->findChild<QObject*>("textOutput");
+    if (true) {
+        std::cout << "Found textOutput" << std::endl;
+        textOutput->setProperty("text", sout);
+    }
 
     return app.exec();
 }
